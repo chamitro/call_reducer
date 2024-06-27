8cb546fc1ce381f173e3027cec8b1c3242beadaf
Thodoris Sotiropoulos <theosotr@aueb.gr>
2024-06-27 16:45:15 +0200

    update universal graph

--- universal.py
+++ universal.py
@@ -1,4 +1,4 @@
-import re
+from typing import NamedTuple, List, Any
 from antlr4 import *
 from SolidityLexer import SolidityLexer
 from SolidityParser import SolidityParser
@@ -6,88 +6,119 @@ from SolidityListener import SolidityListener
 import networkx as nx
 import matplotlib.pyplot as plt
 
+
+class DeclarationNode(NamedTuple):
+    name: str
+    node_type: str
+    parent: Any
+
+    def __hash__(self):
+        return hash((self.name, self.node_type, self.parent))
+
+    def __str__(self):
+        node_name = f"{self.node_type}[{self.name}]"
+        if self.parent is not None:
+            return f"{str(self.parent)}.{node_name}"
+        else:
+            return node_name
+
+    __repr__ = __str__
+
+
 class SolidityGraphListener(SolidityListener):
     def __init__(self):
         super().__init__()
         self.graph = nx.DiGraph()
-        self.contract_counter = 0
         self.function_counter = 0
         self.state_variable_counter = 0
         self.local_variable_counter = 0
-        self.current_contract = None
-        self.current_function = None
+        self.declaration_stack: List[DeclarationNode] = []
+        self.contracts: dict = {}
+
+    def peek_declaration(self):
+        if not self.declaration_stack:
+            return None
+        return self.declaration_stack[-1]
+
+    def push_declaration(self, node: DeclarationNode):
+        self.declaration_stack.append(node)
+
+    def pop_declaration(self):
+        if not self.declaration_stack:
+            return None
+        node = self.declaration_stack[-1]
+        self.declaration_stack = self.declaration_stack[:-1]
+        return node
 
     def enterContractDefinition(self, ctx: SolidityParser.ContractDefinitionContext):
         contract_name = ctx.getChild(1).getText()
-        self.graph.add_node(contract_name, label='contract')
-        self.current_contract = contract_name
-        self.contract_counter += 1
+        contract_node = DeclarationNode(contract_name, "contract", None)
+        self.graph.add_node(contract_node)
+        self.push_declaration(contract_node)
+        self.contracts[contract_node.name] = contract_node
 
         # Handle multiple inheritance specifiers
         if ctx.inheritanceSpecifier():
             inheritance_specifiers = ctx.inheritanceSpecifier()
             for spec in inheritance_specifiers:
                 parent_contract_name = spec.userDefinedTypeName().getText()
-                if parent_contract_name != self.current_contract:
-                    self.add_inherits_edge(parent_contract_name)
+                if parent_contract_name != contract_name:
+                    parent_node = self.contracts[parent_contract_name]
+                    self.graph.add_edge(contract_node, parent_node,
+                                        label="inherits")
 
-    def add_inherits_edge(self, parent_contract_name):
-        if not self.graph.has_edge(self.current_contract, parent_contract_name):
-            self.graph.add_edge(self.current_contract, parent_contract_name, label='inherits')
+    def exitContractDefinition(self, ctx):
+        self.pop_declaration()
 
     def enterFunctionDefinition(self, ctx: SolidityParser.FunctionDefinitionContext):
         functions_name = ctx.getChild(0).getText()
         function_name = functions_name.replace("function", "").strip()
-        unique_function_name = f"{self.current_contract}.{function_name}"
-        self.graph.add_node(unique_function_name, label='function')
-        self.graph.add_edge(self.current_contract, unique_function_name, label='defines_function')
-        self.current_function = unique_function_name
-        self.function_counter += 1
+        parent_node = self.peek_declaration()
+        function_node = DeclarationNode(function_name, "function", parent_node)
+        self.graph.add_node(function_node)
+        self.push_declaration(function_node)
+        if parent_node is not None:
+            self.graph.add_edge(parent_node, function_node, label="def")
+
+    def exitFunctionDefinition(self, ctxt):
+        self.pop_declaration()
 
     def enterEventDefinition(self, ctx: SolidityParser.EventDefinitionContext):
         event_name = ctx.identifier().getText()
-        self.graph.add_node(event_name, label='event')
-        self.graph.add_edge(self.current_contract, event_name, label='defines_event')
-
-    def enterEmitStatement(self, ctx: SolidityParser.EmitStatementContext):
-        emit_statement = ctx.functionCall().getText()
-        unique_emit_statement = f"{self.current_function}.{emit_statement}"
-        self.graph.add_node(unique_emit_statement, label='emit')
-        self.graph.add_edge(self.current_function, unique_emit_statement, label='defines_emit')
+        parent_node = self.peek_declaration()
+        event_node = DeclarationNode(event_name, "event", parent_node)
+        self.graph.add_node(event_node)
+        if parent_node is not None:
+            self.graph.add_edge(parent_node, event_node, label='def')
 
     def enterStructDefinition(self, ctx: SolidityParser.StructDefinitionContext):
         struct_name = ctx.identifier().getText()
-        self.graph.add_node(struct_name, label='struct')
-        self.graph.add_edge(self.current_contract, struct_name, label='defines_struct')
+        parent_node = self.peek_declaration()
+        struct_node = DeclarationNode(struct_name, "struct", parent_node)
+        self.graph.add_node(struct_node)
+        self.push_declaration(struct_node)
+        if parent_node is not None:
+            self.graph.add_edge(parent_node, struct_node, label='def')
 
-        # Handle struct members
-        member_declarations = ctx.variableDeclaration()
-        for member_decl in member_declarations:
-            member_name = member_decl.identifier().getText()
-            unique_member_name = f"{struct_name}.{member_name}"
-            self.graph.add_node(unique_member_name, label='struct_member')
-            self.graph.add_edge(struct_name, unique_member_name, label='defines_struct_member')
+    def exitStructDefinition(self, ctxt):
+        self.pop_declaration()
 
     def enterStateVariableDeclaration(self, ctx: SolidityParser.StateVariableDeclarationContext):
         identifier_token = ctx.identifier()
         var_name = identifier_token.getText()
-        self.graph.add_node(var_name, label='state_variable')
-        self.graph.add_edge(self.current_contract, var_name, label='defines_state_variable')
-        self.state_variable_counter += 1
+        parent_node = self.peek_declaration()
+        var_node = DeclarationNode(var_name, "state_var", parent_node)
+        self.graph.add_node(var_node)
+        if parent_node is not None:
+            self.graph.add_edge(parent_node, var_node, label='def')
 
     def enterVariableDeclaration(self, ctx: SolidityParser.VariableDeclarationContext):
-        if self.current_function:
-            # Check if the variable declaration is directly nested within a struct
-            parent_ctx = ctx.parentCtx
-            if isinstance(parent_ctx, SolidityParser.StructDefinitionContext):
-                # If parent context is StructDefinitionContext, skip adding as local_variable
-                return
-        
-            local_var_name = ctx.getChild(1).getText()
-            unique_local_var_name = f"{self.current_function}.{local_var_name}"
-            self.graph.add_node(unique_local_var_name, label='local_variable')
-            self.graph.add_edge(self.current_function, unique_local_var_name, label='defines_local_variable')
-            self.local_variable_counter += 1
+        local_var_name = ctx.getChild(1).getText()
+        parent_node = self.peek_declaration()
+        var_node = DeclarationNode(local_var_name, "var", parent_node)
+        self.graph.add_node(var_node)
+        if parent_node is not None:
+            self.graph.add_edge(parent_node, var_node, label='def')
 
 
 def build_graph_from_file(file_path):
@@ -105,13 +136,15 @@ def build_graph_from_file(file_path):
 
     return listener.graph
 
+
 if __name__ == '__main__':
     file_path = 'ext_changed.sol'
     graph = build_graph_from_file(file_path)
-    
+
     # Print nodes with specific labels
     def print_nodes_with_label(graph, label):
-        nodes = [node for node, data in graph.nodes(data=True) if data['label'] == label]
+        nodes = [node for node in graph.nodes()
+                 if node.node_type == label]
         print(f"Nodes with label '{label}':")
         print(nodes)
         print()
@@ -129,15 +162,13 @@ if __name__ == '__main__':
     print_nodes_with_label(graph, 'contract')
     print_nodes_with_label(graph, 'function')
     print_nodes_with_label(graph, 'event')
-    print_nodes_with_label(graph, 'emit')
     print_nodes_with_label(graph, 'struct')
-    print_nodes_with_label(graph, 'struct_member')
-    print_nodes_with_label(graph, 'state_variable')
-    print_nodes_with_label(graph, 'local_variable')
-    
+    print_nodes_with_label(graph, 'var')
+    print_nodes_with_label(graph, 'state_var')
+
     # Print edges with labels
     print_edges_with_label(graph)
-    
+
     # Draw and display the graph
     plt.figure(figsize=(12, 8))
     pos = nx.spring_layout(graph, seed=42)  # Positions for all nodes
@@ -145,14 +176,14 @@ if __name__ == '__main__':
     # Nodes
     node_labels = {node: node for node in graph.nodes()}
     node_colors = {'contract': 'lightblue', 'function': 'lightgreen', 'event': 'lightcoral',
-                   'emit': 'lightyellow', 'struct': 'lightgrey', 'struct_member': 'lightgrey',
-                   'state_variable': 'lightpink', 'local_variable': 'lightsalmon'}
+                   'struct': 'lightgrey', 'var': 'yellow',
+                   'state_var': 'lightpink'}
     node_shapes = {'contract': 'o', 'function': 'o', 'event': 'o',
-                   'emit': 'o', 'struct': 's', 'struct_member': 'o',
-                   'state_variable': 'o', 'local_variable': 'o'}
-    
+                   'emit': 'o', 'struct': 's', 'var': 'o',
+                   'state_var': 'o'}
+
     for label in node_colors:
-        nx.draw_networkx_nodes(graph, pos, nodelist=[node for node, data in graph.nodes(data=True) if data['label'] == label],
+        nx.draw_networkx_nodes(graph, pos, nodelist=[node for node in graph.nodes() if node.node_type == label],
                                node_color=node_colors[label], node_shape=node_shapes[label], label=label, node_size=500)
 
     # Edges
